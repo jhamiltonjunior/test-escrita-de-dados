0619e3a69f8cbea0d3d6c7aaf1fa85f060c9c4d1
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-15 07:24:21 -0300

    buscando dados de api externa, tipo struct para users dessa api, funcao para inserir diversas linha no banco de dados, funcao para migrar dados de um banco de dados para outro

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,107 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	connectdb "github.com/jhamiltonjunior/test-escrita-de-dados/model"
+	"io"
+	"net/http"
+	"time"
 )
 
+type User struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"username"`
+}
+
 func main() {
+	initialTime := time.Now()
 	mysql := connectdb.MySQLConnect{}
-	db, err := connectdb.NewConnectionDB(&mysql, "127.0.0.1", "root", "0000", "teste_leitura_de_dados")
+
+	actual := 0
+	ln := 1000
+
+	for i := 0; i < ln; i++ {
+		users := HTTPGet("https://jsonplaceholder.typicode.com/users")
+
+		inserirVariasLinhas(&mysql, users, ln*len(users), &actual)
+	}
+
+	migrarDados(&mysql)
+
+	finalTime := time.Now()
+	fmt.Printf("\nTempo de execucao do Script: %v", finalTime.Sub(initialTime))
+}
+
+func HTTPGet(url string) []User {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(resp.StatusCode)
+
+	body, _ := io.ReadAll(resp.Body)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	var users []User
+
+	err = json.Unmarshal(body, &users)
+	if err != nil {
+		panic(err)
+	}
+
+	return users
+}
+
+func inserirVariasLinhas(mysql *connectdb.MySQLConnect, users []User, ln int, actual *int) {
+	db, err := connectdb.NewConnectionDB(mysql, "127.0.0.1", "root", "0000", "teste_leitura_de_dados")
+	if err != nil {
+		panic(err)
+	}
+	defer func(close *sql.DB) {
+		if err = close.Close(); err != nil {
+			panic(err)
+		}
+	}(db)
+
+	//_, err = db.Exec("DELETE FROM users;")
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	for _, user := range users {
+		var name, email, password = user.Name, user.Email, user.Password
+
+		query := fmt.Sprintf(
+			"INSERT INTO `users`(`name`, `email`, password) VALUES ('%s', '%s', '%s')",
+			name, email, password,
+		)
+
+		_, err = db.Exec(query)
+		if err != nil {
+			panic(err)
+		}
+		*actual++
+		fmt.Printf("\rSalvando %d/%d", *actual, ln)
+	}
+}
+
+func migrarDados(mysql *connectdb.MySQLConnect) {
+	db, err := connectdb.NewConnectionDB(mysql, "127.0.0.1", "root", "0000", "teste_leitura_de_dados")
 	if err != nil {
 		return
 	}
 
-	rows, err := db.Query("SELECT id, name, email FROM users")
+	rows, err := db.Query("SELECT id, name, email, password FROM users;")
 	if err != nil {
 		panic(err)
 	}
@@ -24,37 +113,52 @@ func main() {
 		}
 	}(rows)
 
-	db2, err := connectdb.NewConnectionDB(&mysql, "127.0.0.1", "root", "0000", "teste_escrita_de_dados")
+	totalLinhas := 0
+
+	var users []User
+
+	for rows.Next() {
+		totalLinhas++
+
+		var id int
+		var name, email, password string
+		err := rows.Scan(&id, &name, &email, &password)
+		if err != nil {
+			panic(err)
+		}
+
+		users = append(users, User{id, name, email, password})
+	}
+
+	//fmt.Println(User)
+
+	dbBeWriter, err := connectdb.NewConnectionDB(mysql, "127.0.0.1", "root", "0000", "teste_escrita_de_dados")
 	if err != nil {
 		return
 	}
 
-	password := "securepassword"
-	createAt := "2024-08-14 10:00:00"
-	updateAt := "2024-08-14 10:00:00"
-	deleteAt := "NULL"
-	active := 1
-
-	// Iterando sobre os resultados
-	for rows.Next() {
-		var id int
-		var name, email string
-		err := rows.Scan(&id, &name, &email)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("ID: %d, Name: %s, Email: %s\n", id, name, email)
-
-		query := fmt.Sprintf(
-			"INSERT INTO `users`(`id`, `name`, `email`, `password`, `create_at`, `update_at`, `delete_at`, `active`) VALUES ('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%d')",
-			id, name, email, password, createAt, updateAt, deleteAt, active,
-		)
-
-		_, err = db2.Exec(query)
-		if err != nil {
-			panic(err)
-		}
+	_, err = dbBeWriter.Exec("DELETE FROM users;")
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println("Transferência de dados concluída")
+	actual := 1
+
+	// Iterando sobre os resultados
+	for _, user := range users {
+		var name, email, password = user.Name, user.Email, user.Password
+
+		query := fmt.Sprintf(
+			"INSERT INTO `users`(`name`, `email`, password) VALUES ('%s', '%s', '%s')",
+			name, email, password,
+		)
+
+		_, err = dbBeWriter.Exec(query)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\rSalvando %d/%d", actual, totalLinhas)
+		actual++
+	}
+	fmt.Println("\nTransferência de dados concluída")
 }
