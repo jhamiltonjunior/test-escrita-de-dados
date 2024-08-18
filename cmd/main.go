a32c4d5a9a285ba9b6da5647ec37e624a84f1332
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-18 00:37:15 -0300

    n finalizado como esperado

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,12 @@ import (
 	"fmt"
 	connectdb "github.com/jhamiltonjunior/test-escrita-de-dados/model"
 	"io"
+	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
 	"runtime"
+	"runtime/pprof"
 	"time"
 )
 
@@ -15,28 +19,28 @@ type User struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"username"`
+	Password string `json:"password"` // Corrigi o campo para "password"
 }
 
 func printStatus(totalInserts int, start time.Time) {
 	// Limpa a tela (opcional, dependendo do sistema operacional)
-	fmt.Print("\033[H\033[2J")
-
-	// Tempo de execução
-	elapsed := time.Since(start)
-
-	// Memória utilizada
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	alloc := m.Alloc / 1024 / 1024 // Memória alocada em MB
-
-	// Imprimindo as informações
-	fmt.Printf("Total de Dados Inseridos: %d\n", totalInserts)
-	fmt.Printf("Tempo de Execução: %s\n", elapsed)
-	fmt.Printf("Memória Utilizada: %d MB\n", alloc)
+	//fmt.Print("\033[H\033[2J")
+	//
+	//// Tempo de execução
+	//elapsed := time.Since(start)
+	//
+	//// Memória utilizada
+	//var m runtime.MemStats
+	//runtime.ReadMemStats(&m)
+	//alloc := m.Alloc / 1024 / 1024 // Memória alocada em MB
+	//
+	//// Imprimindo as informações
+	////fmt.Printf("Total de Dados Inseridos: %d\n", totalInserts)
+	////fmt.Printf("Tempo de Execução: %s\n", elapsed)
+	////fmt.Printf("Memória Utilizada: %d MB\n", alloc)
 }
 
-func main() {
+func RunMigration() {
 	done := make(chan struct{})
 	initialTime := time.Now()
 
@@ -71,22 +75,21 @@ func main() {
 	totalInserts := 0
 	go func() {
 		for _ = range done {
-			time.Sleep(100 * time.Millisecond)
-			totalInserts += 1
+			totalInserts++
 
 			printStatus(totalInserts, start)
 		}
 	}()
 
-	actual := 0
-	ln := 1000
-
-	for i := 0; i < ln; i++ {
-		done <- struct{}{}
-		users := HTTPGet("https://jsonplaceholder.typicode.com/users")
-
-		inserirVariasLinhas(dbReader, users, ln*len(users), &actual)
-	}
+	//actual := 0
+	//ln := 1000
+	//
+	////for i := 0; i < ln; i++ {
+	////	done <- struct{}{}
+	////	users := HTTPGet("https://jsonplaceholder.typicode.com/users")
+	////
+	////	inserirVariasLinhas(dbReader, users, ln*len(users), &actual)
+	////}
 
 	migrarDados(dbReader, dbWriter, done)
 
@@ -139,20 +142,39 @@ func inserirVariasLinhas(db *sql.DB, users []User, ln int, actual *int) {
 }
 
 func migrarDados(dbReader, dbBeWriter *sql.DB, done chan struct{}) {
-	rows, err := dbReader.Query("SELECT id, name, email, password FROM users;")
+	rows, err := dbReader.Query("SELECT id, name, email, password FROM users limit 2;")
 	if err != nil {
 		panic(err)
 	}
-	defer func(rows *sql.Rows) {
-		err := rows.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(rows)
 
 	totalLinhas := 0
 
-	var users []User
+	var users chan User
+
+	actual := 1
+
+	go func() {
+		for user := range users {
+
+			var name, email, password = user.Name, user.Email, user.Password
+
+			query := fmt.Sprintf(
+				"INSERT INTO `users`(`name`, `email`, password) VALUES ('%s', '%s', '%s')",
+				name, email, password,
+			)
+
+			_, err = dbBeWriter.Exec(query)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("\rSalvando %d/%d", actual, totalLinhas)
+			actual++
+			done <- struct{}{}
+		}
+
+		close(users)
+		close(done)
+	}()
 
 	for rows.Next() {
 
@@ -160,34 +182,62 @@ func migrarDados(dbReader, dbBeWriter *sql.DB, done chan struct{}) {
 
 		var id int
 		var name, email, password string
-		err := rows.Scan(&id, &name, &email, &password)
+		err = rows.Scan(&id, &name, &email, &password)
 		if err != nil {
 			panic(err)
 		}
 
-		users = append(users, User{id, name, email, password})
+		fmt.Println(users)
+		fmt.Println("users")
+		users <- User{id, name, email, password}
 	}
 
-	actual := 1
-
-	for _, user := range users {
-
-		var name, email, password = user.Name, user.Email, user.Password
-
-		query := fmt.Sprintf(
-			"INSERT INTO `users`(`name`, `email`, password) VALUES ('%s', '%s', '%s')",
-			name, email, password,
-		)
-
-		_, err = dbBeWriter.Exec(query)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("\rSalvando %d/%d", actual, totalLinhas)
-		actual++
-		done <- struct{}{}
+	err = rows.Close()
+	if err != nil {
+		panic(err)
 	}
+
 	fmt.Println("\nTransferência de dados concluída")
+}
 
-	close(done)
+func main() {
+	// Criar arquivo para salvar o perfil de CPU
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+
+		}
+	}(f)
+
+	// Iniciar perfil de CPU
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	memFile, err := os.Create("mem.prof")
+	if err != nil {
+		log.Fatal("could not create MEM profile: ", err)
+	}
+	defer func(f *os.File) {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}(memFile)
+
+	runtime.GC()
+
+	if err = pprof.WriteHeapProfile(memFile); err != nil {
+		log.Fatal("WriteHeapProfile: ", err)
+	}
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	RunMigration()
 }
---
+++ test/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/jhamiltonjunior/test-escrita-de-dados/cmd"
+	"testing"
+)
+
+func TestRunMigration(t *testing.T) {
+	// Aqui você pode adicionar mocks para o banco de dados e testar a função RunMigration
+	main.RunMigration()
+}
